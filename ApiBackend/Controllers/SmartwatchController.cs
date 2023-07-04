944b234c81ed3ef290ee8cc4432c6590c607f321
MarcoAisa <marco.beltrame@stud.itsaltoadriatico.it>
2023-07-04 22:07:45 +0200

    added authorize api and openid config in program

--- ApiBackend/Controllers/SmartwatchController.cs
+++ ApiBackend/Controllers/SmartwatchController.cs
@@ -16,18 +16,23 @@ namespace ApiBackend.Controllers
             _smartwatchService = smartwatchService;
         }
         [HttpGet("/GetSmartwatches")]
+        [Authorize]
         public async Task<List<Smartwatch>> GetSmartwatches(Guid userId, Guid sessionId) =>
             await _smartwatchService.GetSmartwatchAsync(userId, sessionId);
         [HttpGet("/GetAllSmartwatches")]
+        [Authorize]
         public async Task<List<Smartwatch>> GetAllSmartwatches() =>
             await _smartwatchService.GetAllSmartwatchesAsync();
         [HttpGet("/GetSessions")]
+        [Authorize]
         public async Task<List<Session>> GetSessions(Guid userId) =>
             await _smartwatchService.GetSessionsAsync(userId);
         [HttpGet("/GetAllSessions")]
+        [Authorize]
         public async Task<List<Session>> GetAllSessions() =>
             await _smartwatchService.GetAllSessionsAsync();
         [HttpGet("/GetFullSessionsList")]
+        [Authorize]
         public async Task<List<FullSessionViewModel>> GetFullSessionsList(Guid userId) =>
             await _smartwatchService.GetFullSessionAsync(userId);
     }
--- ApiBackend/Program.cs
+++ ApiBackend/Program.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Models;
 using Models.AuthModels;
 using Models.Database;
+using MongoDB.Driver;
+using OpenIddict.Validation.AspNetCore;
+using OpenIddict.Validation.SystemNetHttp;
 using Services.Interfaces;
 using Services.Services;
 
@@ -26,6 +31,70 @@ builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<ISmartwatchServices, SmartwatchServices>();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Earth4Sports API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddOpenIddict()
+    .AddValidation(options =>
+    {
+        options.SetIssuer(new Uri("https://its-cper-projectworky2-gruppo1-auth.azurewebsites.net"));
+     
+        options.AddEncryptionKey(new SymmetricSecurityKey(
+            Convert.FromBase64String("DTjd/GnduI1Kfzen8V8BvaNUfb/VKgXltV7Kbk8iNkY=")));
+
+        // Register the System.Net.Http integration.
+        options.UseSystemNetHttp();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+
+    }).AddCore(o =>
+    {
+        o.UseEntityFrameworkCore().UseDbContext<WatchContext>();
+    });
+
+builder.Services.AddHttpClient(typeof(OpenIddictValidationSystemNetHttpOptions).Assembly.GetName().Name!)
+    .ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    });
+
+builder.Services.AddAuthentication(o => {
+    o.DefaultAuthenticateScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +106,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
